f624bb9449e09fe69bbe6414749678e160c69a46
Muhammad Huzaifa <mhuzaifa3737@gmail.com>
2025-03-02 02:03:43 +0500

    first commit

---
+++ src/utils/fackData/portfolioData.ts
@@ -0,0 +1,26 @@
+export const portfolioData = [
+    {
+        id:1,
+        title:"Mobile Application Design",
+        img:"/images/projects/work2.webp",
+        category:"Branding"
+    },
+    {
+        id:2,
+        title:"Website Makeup Design",
+        img:"/images/projects/work1.webp",
+        category:"Design"
+    },
+    {
+        id:3,
+        title:"Brand Identity and Motion Design",
+        img:"/images/projects/work3.webp",
+        category:"Graphics"
+    },
+    {
+        id:4,
+        title:"Creative Graphics Design",
+        img:"/images/projects/work4.webp",
+        category:"Branding"
+    },
+]
---
+++ src/utils/fackData/servicesData.ts
@@ -0,0 +1,27 @@
+type DataType = {
+    id: number,
+    title: string,
+    img: string,
+    description: string
+}[]
+export const servicesData: DataType = [
+    {
+        id: 1,
+        title: "Product Design",
+        img: "/images/services/service1.webp",
+        description: "Our subscription-based design service gives you access to a dedicated team."
+    },
+    {
+        id: 2,
+        title: "Graphic Design",
+        img: "/images/services/service2.webp",
+        description: "Why settle for one designer when you can have a dedicated team"
+    },
+    {
+        id: 3,
+        title: "Marketing Design",
+        img: "/images/services/service3.webp",
+        description: "How our subscription-based design service with a team"
+    },
+
+]
